a2ba2e9100f45304ea4eb5254b932b6574bd043b
Harshit <sahilchopra838@gmail.com>
2022-06-23 20:08:37 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ function App() {
           React
         </a>
       </header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Learn React Learn React Learn React Learn React Learn
+          React
+        </a>
+      </header>
     </div>
   );
 }
